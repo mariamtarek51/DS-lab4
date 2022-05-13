9a3beea1e8c317fb3ee88811bd375840b1ee2bb5
norah <11Norah>
2022-05-13 03:30:15 +0200

    Uniqueee Random array

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args ) {
 
         int[] S=new int[100];
-        for (int i=0;i<100;i++) {
-             S[i] = new Random().nextInt(50);
+        //Generate unique random array
+        ArrayList<Integer> LIST=new ArrayList<>();
+        for(int j=0;j<100;j++) {
+           LIST.add(j);
         }
+        for (int i=0;i<100;i++) {
+            S[i]= LIST.remove((int)Math.random()*LIST.size());
+
+        }
+
         System.out.println("array : "+java.util.Arrays.toString(S) );
 
         // int S[] ={5,4,3,2,1,9,8,7,6};
-     /*   Nsquaremethod n2=new Nsquaremethod(S);
+       Nsquaremethod n2=new Nsquaremethod(S);
         n2.hashtable();
         System.out.println("Re_building  O(n2): "+n2.getCollisions());
         n2.search(8);
@@ -19,7 +29,7 @@ public class Main {
         n2.search(6);
         n2.search(90);
         n2.search(200);
-        System.out.println(n2.hashtablelength());*/
+        System.out.println("Total spaces in O(n2): "+n2.hashtablelength());
       Nmethod n1=new Nmethod(S);
         n1.FirstLevel();
         n1.search(9);
